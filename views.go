262a06d7d50fcb3876e42d7aa413188c2e873147
Einier Freyre Corona <einierfreyre60@gmail.com>
2026-02-14 11:54:40 -0300

    collapse files

--- views.go
+++ views.go
@@ -6,6 +6,22 @@ import (
 	"github.com/FreyreCorona/SideCar/metrics"
 )
 
+type TextBlock struct {
+	Text string `json:"text"`
+	Size int    `json:"size"`
+}
+
+type ImageBlock struct {
+	Path   string `json:"path"`
+	Width  int    `json:"width"`
+	Height int    `json:"height"`
+}
+
+type RenderFrame struct {
+	Texts  []TextBlock  `json:"texts"`
+	Images []ImageBlock `json:"images"`
+}
+
 func ImageView() RenderFrame {
 	return RenderFrame{
 		Texts: []TextBlock{
